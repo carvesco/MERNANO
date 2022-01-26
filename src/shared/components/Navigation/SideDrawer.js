0f5e0e5d849b4ca03ac7107f07b9631865affdf8
carvesco <cavelascor@unal.edu.co>
2022-01-26 16:39:39 -0500

    :sparkles: side drawer animation

--- src/shared/components/Navigation/SideDrawer.js
+++ src/shared/components/Navigation/SideDrawer.js
@@ -1,9 +1,23 @@
 import React from "react";
+import reactDom from "react-dom";
+import { CSSTransition } from "react-transition-group";
 
 import "./SideDrawer.css";
 
-const SideDrawer = props =>{
-    return <aside className="side-drawer">{props.children}</aside>
+const SideDrawer = (props) => {
+  const content = (
+    <CSSTransition
+      in={props.show}
+      timeout={200}
+      classNames="slide-in-left"
+      mountOnEnter
+      unmountOnExit
+    >
+      <aside className="side-drawer" onClick={props.onClick}>{props.children}</aside>
+    </CSSTransition>
+  );
+
+  return reactDom.createPortal(content, document.getElementById("drawer-hook"));
 };
 
 export default SideDrawer;
