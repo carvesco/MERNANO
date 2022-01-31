3d8d154a9e1158b72dd74ce10a48ffeb7bfb4778
carvesco <cavelascor@unal.edu.co>
2022-01-31 18:45:40 -0500

    :sparkles: added map.js and input for NewPlace.js

---
+++ src/shared/components/UIElements/Map.js
@@ -0,0 +1,11 @@
+import react from "react";
+
+import "./Map.css";
+
+const Map = props =>{
+return <div className={`map ${props.className}`} style={props.style}>
+    
+</div>
+};
+
+export default Map
