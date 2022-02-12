39b13e17e006676e2d1eadfe2ad0aec93b9704dc
carvesco <cavelascor@unal.edu.co>
2022-02-12 10:33:34 -0500

    :sparkles: added context to the app

--- src/shared/components/Navigation/NavLinks.js
+++ src/shared/components/Navigation/NavLinks.js
@@ -1,23 +1,39 @@
-import React from "react";
+import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
 
+import AuthContext from "../../context/auth-context";
 import "./NavLinks.css";
 
-const NavLinks = props =>{
-    return <ul className="nav-links">
+const NavLinks = (props) => {
+  const auth = useContext(AuthContext);
+
+  return (
+    <ul className="nav-links">
+      <li>
+        <NavLink to="/">ALL USERS</NavLink>
+      </li>
+      {auth.isLoggedIn && (
         <li>
-            <NavLink to="/">ALL USERS</NavLink>
+          <NavLink to="/u1/places">MY PLACES</NavLink>
         </li>
+      )}
+      {auth.isLoggedIn && (
         <li>
-            <NavLink to="/u1/places">MY PLACES</NavLink>
+          <NavLink to="/places/new">ADD PLACE</NavLink>
         </li>
-        <li>
-            <NavLink to="/places/new">ADD PLACE</NavLink>
-        </li>
-        <li>
-            <NavLink to="/auth">AUTHENTICATE</NavLink>
-        </li>
+      )}
+       {!auth.isLoggedIn && (
+      <li>
+        <NavLink to="/auth">AUTHENTICATE</NavLink>
+      </li>
+      )}
+      {auth.isLoggedIn && (
+      <li>
+        <NavLink onClick={auth.logout} to="/auth">LOGOUT</NavLink>
+      </li>
+      )}
     </ul>
+  );
 };
 
 export default NavLinks;
